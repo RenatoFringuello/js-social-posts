b800e6a9a7271574c8481dab31d4d26b6ab3fa7e
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-07 15:37:49 +0100

    add create element and create post

---
+++ main.js
@@ -0,0 +1,90 @@
+/**
+ * can create a element using tag, add a class,
+ * append contents and assign attributes
+ * 
+ * @param {*} tag the tag of the element
+ * @param {*} className the whole class list in a string
+ * @param {*} contents a list of content to append
+ * @param {*} attributes a list of attributes to assign; must be an array od literal objects {type:'', value:''}
+ * @returns the element created
+ */
+function createEle(tag, className, contents = [], attributes = []){
+    const element = document.createElement(tag);
+    element.className = className;
+    
+    contents.forEach((content) => {
+        element.append(content);
+    });
+    attributes.forEach((attribute) => {
+        element.setAttribute(attribute.type, attribute.value);
+    });
+
+    return element;
+}
+
+function createPosts(postsList){
+    const post = createEle('div', '', ['ciao']);
+    return post;
+}
+
+//init
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+const container = document.getElementById('container');
+
+container.append(createPosts(posts));
