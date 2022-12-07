a5fc21dacb7ee614e7faccc7abf1d840887fca49
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-07 16:37:33 +0100

    add problem addEventListener

--- main.js
+++ main.js
@@ -94,39 +94,60 @@ const posts = [
 const container = document.getElementById('container');
 
 posts.forEach((post, i) => {
-    const postEle = `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.name} img">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${post.author.name}</div>
-                        <div class="post-meta__time">${post.created}</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">${post.content}</div>
-            <div class="post__image">
-                <img src="${post.media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div> `;
+    //like btn
+    // const likeBtnIcon = createEle('i', 'like-button__icon fas fa-thumbs-up', [], [{type:'aria-hidden', value:'true'}]);
+    // const likeBtnSpan = createEle('span', 'like-button__label', ['Mi Piace']);
+    // const likeBtn = createEle('a', 'like-button  js-like-button', [likeBtnIcon, likeBtnSpan], [{type:'href', value:'#'}]);
+    // //like counter
     // const likeCounter = createEle('b', 'js-likes-counter', ['like count'], [{type:'id', value:'like-counter-' + i}]);
     // const likesCounter = createEle('div', 'likes__counter', [`Piace a ${likeCounter} persone`]);
-
+    
+    const postEle = `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image}" alt="${post.author.name} img">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button" data-postid="${post.id}" id="js-like-button${post.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${post.id}" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div> `;
+    //append to the container
     container.innerHTML += postEle;
+
+    // on like btn click
+    const likeBtn = document.getElementById('js-like-button' + post.id);
+    let isLiked = false;
+    likeBtn.addEventListener('click', function(){
+        // toggle on boolean var
+        isLiked = !isLiked;
+        let like = (isLiked) ? parseInt(post.likes, 10) : parseInt(post.likes, 10) + 1;
+        
+        const likesCounter = document.getElementById(`like-counter-${post.id}`);
+        likesCounter.innerHTML = like;
+        
+        this.classList.toggle('like-button--liked');
+    });
+    console.log(likeBtn);
 });
